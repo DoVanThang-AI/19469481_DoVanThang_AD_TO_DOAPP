b0ce529bbc67fa62614f4b328638e9b6bdc5b7aa
DoVanThang-AI <Dovanthang08072k@gmail.com>
2021-12-23 10:51:52 +0700

    add firebase

---
+++ app/src/main/java/com/a19469481_DoVanThang_AD_ToDoApp/ToDoApp/Model/ToDoModel.java
@@ -0,0 +1,19 @@
+package com.a19469481_DoVanThang_AD_ToDoApp.ToDoApp.Model;
+
+public class ToDoModel extends TaskId {
+
+    private String task , due;
+    private int status;
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
---
+++ app/src/main/java/com/a19469481_DoVanThang_AD_ToDoApp/ToDoApp/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.a19469481_DoVanThang_AD_ToDoApp.ToDoApp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
